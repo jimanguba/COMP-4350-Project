b5ce5d35f7f5384f845d28a8cc659455b060dd5c
Jillian Manguba <manguba3@myumanitoba.ca>
2024-02-21 14:30:19 -0600

    Added routing to Home and Statistics page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("Fetching data....")
+            setLoading(true);
+            try {
+                const { data: response } = await axios.get('http://localhost:5000/books');
+                setData(response);
+            } catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className='container my-5'>
+            <h1 className="text-center mb-4">Books Collection</h1>
+            <div className="table-responsive">
+                <table className='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Pages</th>
+                            <th>Genre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {loading ? (
+                            <tr>
+                                <td colSpan="5" className="text-center">Loading...</td>
+                            </tr>
+                        ) : (
+                            data.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.title}</td>
+                                    <td>{item.author}</td>
+                                    <td>{item.pages}</td>
+                                    <td>{item.genre}</td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
+
