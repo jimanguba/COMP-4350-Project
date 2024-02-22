dd2efd8cbe4853caf1f33a55fea4fc1574d521a8
Ty Brass <tybrass@outlook.com>
2024-02-22 10:38:44 -0600

    Remove unnecessary state

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -3,18 +3,23 @@
  */
 
 import { useState } from "react"
+import { getBooks } from "../lib/requests"
 // _ import BookCoverCard from "......"
 
+
 export default function BookList() {
-    const [books, setBooks] = useState([])
     const [query, setQuery] = useState("")
 
+    // On loading component, request all books,, displayed alphanumerically
+    const books = getBooks()
+
+    // Each list item contains a BookCoverCard, linking to the BookDetailsPage
     return (
         <div>
             <input type="text" placeholder="search titles..." value={query} onChange={e => setQuery(e.target.value)}></input>
             <ul>
                 {books.filter(book => book.title.contains(query)).forEach(() => (
-                    <a href="......."><li>THIS IS WHERE THE BOOK GOES</li></a>
+                    <a href="......."><li>BOOK COVER</li></a>
                 ))}
             </ul>
         </div>
