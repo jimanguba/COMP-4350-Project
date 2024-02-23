61b091e353b8d8b354d1ab52c7e26fa058e4286f
Christopher <euc@myumanitoba.ca>
2024-02-22 21:04:40 -0600

    Added helper book class with some util functions.

---
+++ server/book.js
@@ -0,0 +1,28 @@
+class Book {
+    constructor(name, writer, pageCount, type) {
+        this.title = name,
+        this.author = writer,
+        this.pages = pageCount,
+        this.genre = type
+    };
+    static example = new Book('A Darker Shade of Magic', 'V.E. Schwab', 400, 'Fantasy')
+}
+
+function createNewBook(data) {
+    if(validateBook(data)) {
+        return new Book(data.title, data.author, data.pages, data.genre)
+    }
+    return null
+}
+
+function validateBook(book) {
+    return book?.title && 
+            book?.author &&
+            Number.isInteger(book?.pages) &&
+            book?.genre
+}
+
+module.exports = {
+    createNewBook,
+    validateBook
+};
