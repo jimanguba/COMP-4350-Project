081560a6df055bcd43fb4ec4e61b174a6651d1ef
katew <walleyk1@myumanitoba.ca>
2024-02-27 17:11:02 -0600

    Removing more files

--- client/src/components/CreateAccount.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom"
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import "../style/CreateAccount.css"
