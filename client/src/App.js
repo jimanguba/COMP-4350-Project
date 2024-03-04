c1dfa8b4dee079e9f001b4181f5ed9032dfd84a3
Jillian Manguba <manguba3@myumanitoba.ca>
2024-03-04 10:50:16 -0600

    Deleting old commented-out code for sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -69,32 +69,6 @@ function App() {
             </section>
         </div>
         </Router>
-        // <Router>
-        //     <div className="app-container">
-        //         {/* Sidebar */}
-        //         <div className="sidebar">
-        //             <ul>
-        //                 <li><a href="/">Home</a></li>
-        //                 <li><a href="/statistics">Statistics</a></li>
-        //                 <li><a href="/book-list">Book List</a></li>
-        //                 <li><a href="/review">Review</a></li>
-        //                 {/* Add more sidebar links as needed */}
-        //             </ul>
-        //         </div>
-        //         {/* Main Content */}
-        //         <div className="main-content">
-        //             <Routes>
-        //                 <Route exact path="/" element={<Home />} />
-        //                 <Route path="/statistics" element={<Statistics user_id={1}/>} /> {/* TODO: in sprint 3 - user_id is hardcoded - change user_id to something more dynamic*/}
-        //                 <Route path="/view-book" element={<ViewBook book={{title: "1984", genre: "sci-fi", author: "George Orwell", pages: 172}}/>} />   {/* TODO: similar to the above statistics, this needs to be made dynamic */}
-        //                 <Route path="/book-list" element={<BookList />} />
-        //                 <Route path="/review" element={<ReviewsList />} />
-        //                 <Route path='/Login' element={<Login/>} />
-        //                 {/* Add more routes for additional pages */}
-        //             </Routes>
-        //         </div>
-        //     </div>
-        // </Router>
     )
 };
 
