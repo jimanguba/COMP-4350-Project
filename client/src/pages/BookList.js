04359da8c9fbda978f77425eff5a8996c79acee1
katew <walleyk1@myumanitoba.ca>
2024-04-08 18:27:45 -0500

    Fixing Formatting

--- client/src/pages/BookList.js
+++ client/src/pages/BookList.js
@@ -45,21 +45,13 @@ export default function BookList ({ type }) {
       <Sidebar />
       <div className='bookList'>
         {
-          type === 'all' 
-          ?(
-            <h1>All Books</h1>
-          )
-          : type === 'completed' 
-          ?(
-            <h1>Completed Books</h1>
-          ) 
-          : type === 'to-read' 
-          ?(
-            <h1>Want To Read</h1>
-          ) 
-          : (
-            <h1>Books</h1>
-          )
+          type === 'all'
+            ? ( <h1>All Books</h1> )
+            : type === 'completed' 
+            ? ( <h1>Completed Books</h1> ) 
+            : type === 'to-read' 
+            ? ( <h1>Want To Read</h1> ) 
+            : ( <h1>Books</h1> )
         }
         <input
           type='text'
